1a375b4e2feb8740fd06ff5534691b6d08b10410
rihanas <rihanna.socials@gmail.com>
2025-09-14 02:14:22 +0300

    task 2

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -18,3 +18,19 @@ const teacher3: Teacher = {
 };
 
 console.log(teacher3);
+
+// Directors interface extends Teacher
+interface Directors extends Teacher {
+  numberOfReports: number; // required attribute
+}
+
+// Example usage
+const director1: Directors = {
+  firstName: 'John',
+  lastName: 'Doe',
+  location: 'London',
+  fullTimeEmployee: true,
+  numberOfReports: 17,
+};
+
+console.log(director1);
