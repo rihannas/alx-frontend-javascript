db6c788faf5f44175b7b5320d0d79e95cbb3e019
rihanas <rihanna.socials@gmail.com>
2025-09-14 17:35:24 +0300

    fix

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -44,8 +44,11 @@ class Teacher implements TeacherInterface {
 
 // createEmployee function
 function createEmployee(salary: number | string): Director | Teacher {
-  if (typeof salary === 'number' && salary < 500) {
-    return new Teacher();
+  if (typeof salary === 'number') {
+    if (salary < 500) {
+      return new Teacher();
+    }
+    return new Director();
   }
   return new Director();
 }
