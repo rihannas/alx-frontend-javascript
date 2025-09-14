b72d463cf5e5816b960809666b834aab798e2388
rihanas <rihanna.socials@gmail.com>
2025-09-14 17:57:35 +0300

    task 8 fix

--- 0x04-TypeScript/task_3/js/main.ts
+++ 0x04-TypeScript/task_3/js/main.ts
@@ -3,22 +3,18 @@
 import { RowID, RowElement } from './interface';
 import * as CRUD from './crud.js';
 
-// Create a row object
 const row: RowElement = {
   firstName: 'Guillaume',
   lastName: 'Salva',
 };
 
-// Insert the row and get the new row ID
+// Insert the row
 const newRowID: RowID = CRUD.insertRow(row);
 
-// Create an updated row with age added
-const updatedRow: RowElement = {
-  ...row,
-  age: 23,
-};
+// Update the row with age using spread syntax
+const updatedRow: RowElement = { ...row, age: 23 };
 
-// Update the row in the database
+// Update the database
 CRUD.updateRow(newRowID, updatedRow);
 
 // Delete the row
